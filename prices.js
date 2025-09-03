1d3dac6f8d54b644cefceea2ca0f269ec0839311
chani0124 <79517863+chani0124@users.noreply.github.com>
2025-09-03 18:21:43 +0900

    Add files via upload

---
+++ prices.js
@@ -0,0 +1,21 @@
+
+const prices = [
+  {
+    site: "쿠팡",
+    title: "몽베스트 생수 2L x 12개",
+    price: "11,900원",
+    url: "https://www.coupang.com/vp/products/1234567890"
+  },
+  {
+    site: "11번가",
+    title: "몽베스트 생수 500ml x 20개",
+    price: "8,900원",
+    url: "https://www.11st.co.kr/products/987654321"
+  },
+  {
+    site: "다나와",
+    title: "몽베스트 생수 2L x 6개",
+    price: "6,900원",
+    url: "https://search.danawa.com/dsearch.php?k1=몽베스트+생수"
+  }
+];
